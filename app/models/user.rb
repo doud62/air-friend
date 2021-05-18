884628c25adaacecca28800200c1750c253d39d8
Gabriel Ottmann gaby.ottmann@gmail.com <gaby.ottmann@gmail.com>
2021-05-18 15:38:46 +0200

    ajout validations sur user

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,10 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+  validates :first_name, presence: true
+  validates :last_name, presence: true
+  validates :age, presence: true
+  validates :user_description, presence: true
+  validates :user_email, presence: true
+  validates :user_password, presence: true
 end
