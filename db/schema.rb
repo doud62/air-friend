d39c2630f52a08e32533a3bcc0c43ee27d368e6f
Edouard Bernard <edouard.bernard@live.fr>
2021-05-18 15:06:02 +0200

    Add users and friends references to Bookings

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_05_18_122259) do
+ActiveRecord::Schema.define(version: 2021_05_18_125727) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -22,6 +22,10 @@ ActiveRecord::Schema.define(version: 2021_05_18_122259) do
     t.integer "status"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.bigint "user_id", null: false
+    t.bigint "friend_id", null: false
+    t.index ["friend_id"], name: "index_bookings_on_friend_id"
+    t.index ["user_id"], name: "index_bookings_on_user_id"
   end
 
   create_table "friends", force: :cascade do |t|
@@ -53,4 +57,6 @@ ActiveRecord::Schema.define(version: 2021_05_18_122259) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
+  add_foreign_key "bookings", "friends"
+  add_foreign_key "bookings", "users"
 end
