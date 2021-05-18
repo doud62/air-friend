aa59114b94a46139ce4bcb169d32506e19411327
Edouard Bernard <edouard.bernard@live.fr>
2021-05-18 14:28:08 +0200

    Booking model

--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,19 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_05_18_092451) do
+ActiveRecord::Schema.define(version: 2021_05_18_122259) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "bookings", force: :cascade do |t|
+    t.date "date"
+    t.integer "duration"
+    t.string "address"
+    t.integer "status"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
 
   create_table "friends", force: :cascade do |t|
     t.string "first_name"
