590510ad2a147340298eccaa6aea77a9114a16d0
Gabriel Ottmann gaby.ottmann@gmail.com <gaby.ottmann@gmail.com>
2021-05-18 15:25:39 +0200

    ajout validations sur friends

--- app/models/friend.rb
+++ app/models/friend.rb
@@ -1,3 +1,11 @@
 class Friend < ApplicationRecord
-
+  belongs_to :user
+  validates :first_name, presence: true
+  validates :last_name, presence: true
+  validates :age, presence: true
+  validates :gender, presence: true
+  validates :friend_description, presence: true
+  validates :address, presence: true
+  validates :price, presence: true
+  validates :image_url, presence: true
 end
