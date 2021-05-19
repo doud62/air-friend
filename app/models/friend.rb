16ef26278a71d3b11b945fa1ce4a0e8aeb3e5919
Alizee Brignonen <alizee.brignonen@essca.eu>
2021-05-19 12:43:32 +0200

    View Friends + Seed

--- app/models/friend.rb
+++ app/models/friend.rb
@@ -1,9 +1,10 @@
 class Friend < ApplicationRecord
   belongs_to :user
+  has_many :bookings
   validates :first_name, presence: true
   validates :last_name, presence: true
   validates :description, presence: true
   validates :address, presence: true
   validates :price, presence: true
-  has_many :bookings
+
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_05_18_125727) do
+ActiveRecord::Schema.define(version: 2021_05_19_094329) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -39,6 +39,8 @@ ActiveRecord::Schema.define(version: 2021_05_18_125727) do
     t.boolean "availability"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.bigint "user_id", null: false
+    t.index ["user_id"], name: "index_friends_on_user_id"
   end
 
   create_table "users", force: :cascade do |t|
@@ -59,4 +61,5 @@ ActiveRecord::Schema.define(version: 2021_05_18_125727) do
 
   add_foreign_key "bookings", "friends"
   add_foreign_key "bookings", "users"
+  add_foreign_key "friends", "users"
 end
